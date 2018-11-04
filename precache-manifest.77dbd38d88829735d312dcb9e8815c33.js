0ef503912c88c81474a1835dca763dfa8fc23844
lfreitasc <lucas.freitascosta96@gmail.com>
2018-11-03 22:23:45 -0300

    Updates

---
+++ precache-manifest.77dbd38d88829735d312dcb9e8815c33.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f187b1bb2d16dcbc673e3221a56fcc",
+    "url": "/lfreitasc/static/media/facebook.17f187b1.svg"
+  },
+  {
+    "revision": "6e7f09a1e1c7e13ef049",
+    "url": "/lfreitasc/static/css/main.ad87930a.chunk.css"
+  },
+  {
+    "revision": "bf75b54e4abb4361c5e3",
+    "url": "/lfreitasc/static/js/1.bf75b54e.chunk.js"
+  },
+  {
+    "revision": "9568b0f569816b624428",
+    "url": "/lfreitasc/static/js/runtime~main.9568b0f5.js"
+  },
+  {
+    "revision": "0cc2f260be26d30cba48e35fffe12a28",
+    "url": "/lfreitasc/static/media/cv.0cc2f260.pdf"
+  },
+  {
+    "revision": "add0c81660c3ba53de361fcace6fb4ac",
+    "url": "/lfreitasc/static/media/curriculum.add0c816.svg"
+  },
+  {
+    "revision": "6e7f09a1e1c7e13ef049",
+    "url": "/lfreitasc/static/js/main.6e7f09a1.chunk.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/lfreitasc/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "753c8e94a41718a5d026c21ba5af7264",
+    "url": "/lfreitasc/static/media/profile.753c8e94.jpg"
+  },
+  {
+    "revision": "b1c69fe0f2b1a225af1c2a8539787d16",
+    "url": "/lfreitasc/static/media/ingles.b1c69fe0.pdf"
+  },
+  {
+    "revision": "341351c35d4502b6da8e3253a958b03b",
+    "url": "/lfreitasc/static/media/notapp.341351c3.jpg"
+  },
+  {
+    "revision": "6bef3114c35a185381c458c65baad224",
+    "url": "/lfreitasc/static/media/bazar.6bef3114.jpg"
+  },
+  {
+    "revision": "dd83e5da8835577fb3b409286c4b7fe1",
+    "url": "/lfreitasc/index.html"
+  }
+];
